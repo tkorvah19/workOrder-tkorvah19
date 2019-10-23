b37b5d34d666646177b302df41107c88bd5a1073
tkorvah19 <koivogui2@gmail.com>
2019-10-22 21:57:43 -0500

    Validation Error Checks

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+//require_once('model/User.php');
+//require_once('model/database.php');
+//require_once('model/UserDB.php');
+//require_once('model/fitnessData_db.php');
+//require_once('model/Fitness.php');
+session_start();
+
+
+?>
