38db77bd960728b35c609c4ec8d17e038700a0d0
tkorvah19 <koivogui2@gmail.com>
2019-10-25 08:07:08 -0500

    Home Page StyleSheet Index

---
+++ view/Home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--
+Project: Work Order System
+Programmer: Tarnue Korvah
+Date: 10/25/2019
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+          <link rel="stylesheet" type="text/css" href="stylesheet.css">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        
+        <header>
+            Welcome to the Work Order Management System
+        </header>
+        <main>
+            
+            <article>
+                
+            </article>
+            <nav>
+                <div>Home</a></div>
+                 <div>Administration</div>
+                  <div>Engineer</div>
+                   <div>Employee</div>
+            </nav>
+            
+        </main>
+        <footer>
+            <p class="copyright">
+        &copy; <?php echo date("Y"); ?> Korvah's Project, Inc.
+    </p>
+        </footer>
+    </body>
+</html>
